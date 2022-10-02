17d3dc64b76e902dffe3d276d1e75a9b179feedd
David Leonel <davidx6x@hotmail.com>
2022-10-02 12:21:41 -0300

    Primera pagina de Carolina Spa

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+window.swiper = new Swiper({
+    el: '.slider__contenedor',
+    slideClass: 'slider__slide',
+    createElements: true,
+    autoplay: {
+        delay: 3500
+    },
+    loop: true,
+    pagination: true,
+    navigation: true
+});
